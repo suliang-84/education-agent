f0c579162f4cd15e9ce66f6a320d101e2c86f34b
suliang <942055956@qq.com>
2026-09-05 14:12:43 +0800

    feat:前端框架

--- code/manage-frontend/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<style>
-* { box-sizing: border-box; margin: 0; padding: 0; }
-body { font-family: -apple-system, BlinkMacSystemFont, 'PingFang SC', 'Segoe UI', sans-serif; }
-</style>
