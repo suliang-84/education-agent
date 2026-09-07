83e89bb2e45c7a8b145c678a8657015e6ddad218
suliang <942055956@qq.com>
2026-09-07 23:24:17 +0800

    feat: 登录页

--- code/manage-frontend/src/views/login/LoginView.vue
+++ code/manage-frontend/src/views/login/LoginView.vue
@@ -2,7 +2,7 @@
   <div class="login-root">
 
     <!-- ── 背景视频 ── -->
-    <video class="bg-video" autoplay muted playsinline>
+    <video class="bg-video" autoplay muted playsinline loop>
       <source src="@/assets/login-background.mp4" type="video/mp4" />
     </video>
     <div class="bg-overlay" />
